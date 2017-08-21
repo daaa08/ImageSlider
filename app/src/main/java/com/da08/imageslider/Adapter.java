c74dfb5ace3e0c7752f6070af0be03310b0d78d4
박다정 <daaa08@naver.com>
2017-08-21 21:22:09 +0900

    Initial commit

---
+++ app/src/main/java/com/da08/imageslider/Adapter.java
@@ -0,0 +1,53 @@
+package com.da08.imageslider;
+
+import android.content.Context;
+import android.support.v4.view.PagerAdapter;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+/**
+ * Created by Da08 on 2017. 8. 21..
+ */
+
+public class Adapter extends PagerAdapter {
+
+    private int[] imgs = {R.drawable.one, R.drawable.two, R.drawable.three};  // img 담기
+    private LayoutInflater inflater;
+    private Context context;
+
+    public Adapter(Context context){
+        this.context = context;
+    }
+
+    @Override
+    public int getCount() {   // img 개수
+        return imgs.length;
+
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object object) {
+        return view == ((LinearLayout) object);
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {   // 각각의 아이템 인스턴스화
+        inflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View v = inflater.inflate(R.layout.slider, container, false);
+        ImageView imageView = v.findViewById(R.id.imageView);
+        TextView textView = v.findViewById(R.id.textView);
+        imageView.setImageResource(imgs[position]);
+        textView.setText((position +1)+"번째 이미지");
+        container.addView(v);  // 해당 뷰 추가
+        return v;  // 뷰 반환
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {  // 아이템 객체 파괴
+        container.invalidate();
+    }
+}
